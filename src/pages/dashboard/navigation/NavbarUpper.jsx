2d8b3ea919da47abb5bcee1e60316dbcd243c358
ThakurSaad <thakursaad613@gmail.com>
2024-06-30 20:03:11 +0600

    dashboard nav dropdown fix

--- src/pages/dashboard/navigation/NavbarUpper.jsx
+++ src/pages/dashboard/navigation/NavbarUpper.jsx
@@ -1,12 +1,18 @@
 import { BsList } from "react-icons/bs";
 import logo from "../../../assets/nano-worker-logo.svg";
 import { FaBell, FaCoins } from "react-icons/fa";
-import { IoIosArrowDropdown } from "react-icons/io";
+import { IoIosArrowDropdown, IoIosArrowDropup } from "react-icons/io";
 import useUser from "../../../hooks/useUser";
 import defaultDp from "../../../assets/dashboard-default-dp.jpg";
+import { useState } from "react";
 
 const NavbarUpper = () => {
   const { user } = useUser();
+  const [open, setOpen] = useState(true);
+
+  const handleIcon = () => {
+    setOpen(!open);
+  };
 
   return (
     <div>
@@ -17,10 +23,16 @@ const NavbarUpper = () => {
         <div className="flex flex-grow justify-evenly xs:flex-none md:mr-5">
           {/* profile */}
           <div className="dropdown -mb-2">
-            <div className="avatar">
-              <div className="absolute z-50 bg-transparent w-7 h-7 p-0 m-0 right-0 top-0 -mt-1 -mr-5">
-                <IoIosArrowDropdown className="text-xl text-orange-600" />
-              </div>
+            <div onClick={handleIcon} className="avatar">
+              {!open ? (
+                <div className="absolute z-50 bg-transparent w-7 h-7 p-0 m-0 right-0 top-0 -mt-1 -mr-5">
+                  <IoIosArrowDropdown className="text-xl text-orange-600" />
+                </div>
+              ) : (
+                <div className="absolute z-50 bg-transparent w-7 h-7 p-0 m-0 right-0 top-0 -mt-1 -mr-5">
+                  <IoIosArrowDropup className="text-xl text-orange-600" />
+                </div>
+              )}
               <div
                 className="border-2 border-gray-200 hover:scale-105 duration-75 hover:cursor-pointer w-12 sm:w-14 rounded-full"
                 tabIndex={2}
@@ -28,21 +40,25 @@ const NavbarUpper = () => {
                 <img src={defaultDp} />
               </div>
             </div>
-            <ul
-              tabIndex={2}
-              className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-28 bg-orange-50`}
-            >
-              <small className="text-sm text-customOrange">
-                {user.display_name}
-              </small>
-              <small className="text-sm">{user.role}</small>
-              <small className="text-sm flex flex-row">
-                {user.coin}
-                <span>
-                  <FaCoins className="pl-2 text-2xl" />
-                </span>
-              </small>
-            </ul>
+            {!open ? (
+              <ul
+                tabIndex={2}
+                className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-28 bg-orange-50`}
+              >
+                <small className="text-sm text-customOrange">
+                  {user.display_name}
+                </small>
+                <small className="text-sm">{user.role}</small>
+                <small className="text-sm flex flex-row">
+                  {user.coin}
+                  <span>
+                    <FaCoins className="pl-2 text-2xl" />
+                  </span>
+                </small>
+              </ul>
+            ) : (
+              ""
+            )}
           </div>
 
           {/* notification */}
